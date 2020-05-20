3a45736edf60d00abb7b07136eb3032f8cd07c17
sohampatelk <sohampatelk@yahoo.com>
2020-05-20 15:10:55 -0600

    may20-netcore-todo-warren

---
+++ Todo/Todo/Models/ToDoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Task Required")]
+        public string Task { get; set; }
+        /*[Required]
+        public string Task2 { get; set; }*/
+        public bool Completed { get; set; }
+    }
+}
